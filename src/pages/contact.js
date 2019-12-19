4cbc2ca17d620912cbc5e4e885d9bb233977ec51
industriousparadigm <dsgmcosta@gmail.com>
2019-12-19 09:32:16 +0000

    dynamic blog posts generated

---
+++ src/pages/contact.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <h1>Contact</h1>
+      <p>
+        Find me musing around on the twitter{" "}
+        <a
+          href="https://twitter.com/@diogocosta_eu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @diogocosta_eu
+        </a>{" "}
+      </p>
+    </Layout>
+  )
+}
+
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+import Layout from "../components/layout"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <h1>Hello.</h1>
+      <h2>
+        I'm Diogo, a full-stack developer living in somber but fun London.
+      </h2>
+      <p>
+        Need a developer? <Link to="/contact">Contact me</Link>.
+      </p>
+    </Layout>
+  )
+}
+
+export default IndexPage
